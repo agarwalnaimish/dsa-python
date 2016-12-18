e17f76af2c87912c17548d80e6aa4c6237806dc7
Naimish Agarwal <agarwalnaimish@gmail.com>
2016-12-18 07:22:04 +0530

    updated definition of fundamental data structures

---
+++ fundamental_data_structures.py
@@ -0,0 +1,56 @@
+"""
+Fundamental Data Structures like Bag, Queue and Stack.
+
+@author: agarwalnaimish
+"""
+
+
+class Bag(object):
+
+    def __init__(self):
+        pass
+
+    def add(self, item):
+        pass
+
+    def is_Empty(self):
+        pass
+
+    def size(self):
+        pass
+
+
+class Queue(object):
+
+    def __init__(self):
+        pass
+
+    def enqueue(self, item):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def size(self):
+        pass
+
+
+class Stack(object):
+
+    def __init__(self):
+        pass
+
+    def push(self, item):
+        pass
+
+    def pop(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def size(self):
+        pass
